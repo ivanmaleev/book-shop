e8e1ba87c4fd2e10f1206864f2b0bb3325fd4c96
Ivan Maleev <ivanmaleev@inbox.ru>
2023-06-11 15:07:53 +0300

    Banner

--- src/main/java/com/example/bookshop/BookShopApplication.java
+++ src/main/java/com/example/bookshop/BookShopApplication.java
@@ -2,10 +2,14 @@ package com.example.bookshop;
 
 import com.example.bookshop.service.LoadGenresService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.core.env.Environment;
 import org.springframework.scheduling.annotation.EnableAsync;
+
+import java.io.PrintStream;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy(proxyTargetClass = true)
@@ -17,7 +21,21 @@ public class BookShopApplication {
     LoadGenresService loadGenresService;
 
     public static void main(String[] args) {
-        SpringApplication.run(BookShopApplication.class, args);
+        SpringApplication springApplication = new SpringApplication(BookShopApplication.class);
+        springApplication.setBanner((environment, sourceClass, out) ->
+                out.println("\n" +
+                        " /$$$$$$$                      /$$                       /$$                          \n" +
+                        "| $$__  $$                    | $$                      | $$                          \n" +
+                        "| $$  \\ $$  /$$$$$$   /$$$$$$ | $$   /$$        /$$$$$$$| $$$$$$$   /$$$$$$   /$$$$$$ \n" +
+                        "| $$$$$$$  /$$__  $$ /$$__  $$| $$  /$$/       /$$_____/| $$__  $$ /$$__  $$ /$$__  $$\n" +
+                        "| $$__  $$| $$  \\ $$| $$  \\ $$| $$$$$$/       |  $$$$$$ | $$  \\ $$| $$  \\ $$| $$  \\ $$\n" +
+                        "| $$  \\ $$| $$  | $$| $$  | $$| $$_  $$        \\____  $$| $$  | $$| $$  | $$| $$  | $$\n" +
+                        "| $$$$$$$/|  $$$$$$/|  $$$$$$/| $$ \\  $$       /$$$$$$$/| $$  | $$|  $$$$$$/| $$$$$$$/\n" +
+                        "|_______/  \\______/  \\______/ |__/  \\__/      |_______/ |__/  |__/ \\______/ | $$____/ \n" +
+                        "                                                                            | $$      \n" +
+                        "                                                                            | $$      \n" +
+                        "                                                                            |__/      \n"));
+        springApplication.run(args);
     }
 
 //    @EventListener(ApplicationReadyEvent.class)
